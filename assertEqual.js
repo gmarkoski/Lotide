15a41b0d230e40d20ecb15287999ecd48f32ed87
gmarkoski <gary_markoski@hotmail.com>
2021-11-22 21:26:02 -0700

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let valA = actual;
+  let valB = expected;
+
+  if (valA === valB) {
+    console.log("\uD83D\uDE00 \uD83D\uDE00 \uD83D\uDE4F Assertion Passed: [valA] === [valB]");
+  } else {
+    console.log("Assertion Failed: [valA] !=== [valB]");
+  }
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
